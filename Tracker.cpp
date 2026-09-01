c1569e2425e029ca77bd56fe4be063cd6f849ee9
James <irbiddlecombe@gmail.com>
2026-09-01 23:01:11 +0200

    Add Tracker class with tracking functionality
    Implement Tracker class with methods for initialization and animal tracking.

---
+++ Tracker.cpp
@@ -0,0 +1,53 @@
+#include "Tracker.h"
+#include "config.h"
+
+bool Tracker::begin()
+{
+    Serial.println("[TRACKER] Initialising tracker");
+
+    return true;
+}
+
+bool Tracker::update(Animal& animal)
+{
+#if MOCK_TRACKING
+
+    return getMockAnimal(animal);
+
+#else
+
+    return getMovebankAnimal(animal);
+
+#endif
+}
+
+bool Tracker::getMockAnimal(Animal& animal)
+{
+    animal.name = "Luna";
+    animal.species = "King Penguin";
+
+    animal.description =
+        "A tracked wildlife friend";
+
+    animal.location.latitude = -54.8123;
+    animal.location.longitude = 158.4231;
+
+    animal.location.locationName =
+        "Southern Ocean";
+
+    animal.location.timestamp =
+        millis();
+
+    animal.location.valid = true;
+
+    animal.distanceTravelledKm = 1284.7;
+
+    return true;
+}
+
+bool Tracker::getMovebankAnimal(Animal& animal)
+{
+    // Movebank implementation will go here.
+
+    return false;
+}
